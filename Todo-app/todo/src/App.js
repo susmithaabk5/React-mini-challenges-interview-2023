d990272eb65fb9194633d680be90c66b9c08a114
susmithaa <susmithaajob@gmail.com>
2023-05-25 14:09:40 -0700

    edit

--- Todo-app/todo/src/App.js
+++ Todo-app/todo/src/App.js
@@ -5,6 +5,9 @@ import "./App.css";
 function App() {
   const [text, setText] = useState("");
   const [items, setItems] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editText, setEditText] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text.trim() !== "") {
@@ -17,6 +20,21 @@ function App() {
     spreadItem.splice(index, 1);
     setItems(spreadItem);
   };
+  const handleEdit = (i) => {
+    setEditIndex(i);
+    setEditText(items[i]);
+  };
+  const updateList = (i) => {
+    const newupdate = [...items];
+    newupdate[i] = editText;
+    setEditText(newupdate);
+    setEditIndex(null);
+    setEditText("");
+  };
+  const cancel = () => {
+    setEditIndex(null);
+  };
+
   return (
     <div className="App">
       <div className="block">
@@ -30,11 +48,30 @@ function App() {
       {items.length === 0 ? (
         <p>No items added to the cart</p>
       ) : (
-        items.map((data, i) => {
+        items.map((data, index) => {
           return (
-            <div className="block" key={i}>
-              <li>{data}</li>
-              <button onClick={(i) => handleDelete(i)}>Delete</button>
+            <div className="block">
+              <li key={index}>
+                {editIndex === index ? (
+                  <form>
+                    <input
+                      type="text"
+                      value={editText}
+                      onChange={(e) => {
+                        setEditText(e.target.value);
+                      }}
+                    ></input>
+                    <button onClick={() => updateList(index)}>update</button>
+                    <button onClick={() => cancel(index)}>cancel</button>
+                  </form>
+                ) : (
+                  <>
+                    {data}
+                    <button onClick={() => handleEdit(index)}>Edit</button>
+                    <button onClick={() => handleDelete(index)}>Delete</button>
+                  </>
+                )}
+              </li>
             </div>
           );
         })
